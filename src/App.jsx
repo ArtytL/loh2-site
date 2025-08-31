ce293854b28a4a1702e44457b956c488ccf9478a
ArtytL <artyt.sun@gmail.com>
2025-08-31 15:35:34 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-// ... import อื่น ๆ
-import Product from "./pages/Product.jsx";
+// src/App.jsx
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Catalog from "./pages/Catalog.jsx";
+import Checkout from "./pages/Checkout.jsx";
+import Transfer from "./pages/Transfer.jsx";
+import Admin from "./pages/Admin.jsx";
 
-// ...
-<Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/catalog" element={<Catalog />} />
-  <Route path="/checkout" element={<Checkout />} />
-  <Route path="/transfer" element={<Transfer />} />
-  <Route path="/admin" element={<Admin />} />
-  <Route path="/product/:id" element={<Product />} />  {/* << เพิ่มบรรทัดนี้ */}
-  <Route path="*" element={<Navigate to="/" replace />} />
-</Routes>
+export default function App() {
+  return (
+    <>
+      {/* header / nav ของคุณ */}
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/transfer" element={<Transfer />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
+    </>
+  );
+}
