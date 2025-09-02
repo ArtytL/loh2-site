e09df019a510856376a03e256a3ff94a59e665e7
ArtytL <artyt.sun@gmail.com>
2025-09-03 03:35:37 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
 // src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
-import './index.css';
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
+  // เอา StrictMode ออกชั่วคราวเพื่อตัด side-effect ซ้ำ
+  <App />
 );
