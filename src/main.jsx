90310fd21fd2d9bf89af6bcf6126d8c4ecc434aa
ArtytL <artyt.sun@gmail.com>
2025-09-03 03:51:44 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
-// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  // เอา StrictMode ออกชั่วคราวเพื่อตัด side-effect ซ้ำ
-  <App />
-);
+createRoot(document.getElementById("root")).render(<App />);
